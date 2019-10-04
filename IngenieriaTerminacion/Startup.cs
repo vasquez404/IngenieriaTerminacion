3f13d21f4ff173b58e069942994366c61ed0c136
Diego Vasquez <diegovasquezcolombia@gmail.com>
2019-10-04 15:57:35 -0500

    Agregar archivos de proyecto.

---
+++ IngenieriaTerminacion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IngenieriaTerminacion.Startup))]
+namespace IngenieriaTerminacion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
